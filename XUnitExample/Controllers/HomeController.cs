390507c8a9e9cfe6819f4f9570fe0e21443cd7c3
bapuji <babuji.cybrain@gmail.com>
2023-10-13 11:14:45 +0530

    first-commit

---
+++ XUnitExample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XUnitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "I am in Home.";
+        }
+        [HttpGet("CheckNumber/{guessedNumber}")]
+        public string CheckNumber(int guessedNumber)
+        {
+            if (guessedNumber > 100)
+            {
+                return "Worng ! try Smaller Number.";
+            }
+            else if (guessedNumber < 100)
+            {
+                return "Worng ! try Greter Number.";
+            }
+            else
+            {
+                return "You guessed correct number.";
+            }
+        }
+    }
+}
---
+++ xUnitExample.Tests/Controlers/HomeControlerTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XUnitExample.Controllers;
+
+namespace xUnitExample.Tests.Controlers
+{
+    public class HomeControlerTest
+    {
+        [Fact]
+        public void HomeControler_Index_ValidResult()
+        {
+            ///AAA
+
+            ///Arange -
+            HomeController controller = new HomeController();
+            string expectedResult = "I am in Home.";
+
+            ///Add
+            string result = controller.Index();
+
+            ///Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(101, "Worng ! try Smaller Number.")]
+        [InlineData(99, "Worng ! try Greter Number.")]
+        [InlineData(100, "You guessed correct number.")]
+        public void HomeControler_CheckNumber_ValidResult(int guessedNumber, string expectedMessage)
+        {
+            // Arrange
+            var controller = new HomeController(); 
+
+            // Act
+            var result = controller.CheckNumber(guessedNumber);
+
+            // Assert
+            Assert.Equal(expectedMessage, result);
+        }
+        [Theory]
+        [ClassData(typeof(CheckNumberTestData))]
+        public void CheckNumber_ReturnsExpectedResult(int guessedNumber, string expectedMessage)
+        {
+            // Arrange
+            var controller = new HomeController(); // Create an instance of your controller
+
+            // Act
+            var result = controller.CheckNumber(guessedNumber);
+
+            // Assert
+            Assert.Equal(expectedMessage, result);
+        }
+    }
+    public class CheckNumberTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 101, "Worng ! try Smaller Number." };
+            yield return new object[] { 99, "Worng ! try Greter Number." };
+            yield return new object[] { 100, "You guessed correct number." };
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
